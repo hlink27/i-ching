585b190d602964f6356a8053e7f62c5f5d1b3fec
Hierro <79893038+hlink27@users.noreply.github.com>
2022-02-01 00:25:15 -0300

    Update iching.py

--- iching.py
+++ iching.py
@@ -1,6 +1,14 @@
+from colorama import Fore, Back, Style
 i, j, seq, seq2 = 0, 6, [], []
+
+#Display Fo-Hi
+f = open("yinyang.txt", "r")
+print(f.read())
+f.close()
+
 print("Toss three coins at the same time six times, answering the head or tails question." )
 while i < j:
+    print("Throw #", (i+1))
     head = int(input("Number of coins that turned head: "))
     tails = int(input("Number of coins that turned tails: "))
     if head + tails == 3:
@@ -10,31 +18,32 @@ while i < j:
         seq.append(line)
         i = i +1
     else:
-        print("You need to toss THREE coins.")
+        print(Fore.RED + " '\033[1m' You need to toss THREE coins. '\033[1m'")
+        print(Style.RESET_ALL)
     print("")
 
 #Present Hexagram
 print("Present Hexagram:")
 for x in seq:
     if x == 9:
-        h = ("___")
+        h = (" ________________\n/________________/")
     elif x == 8:
-        h = ("_ _")    
+        h = (" _______   _______\n/_______/ /_______/")    
     elif x == 7:
-         h = ("___")
+         h = (" ________________\n/________________/")
     else:
-        h = ("_ _")
+        h = (" _______   _______\n/_______/ /_______/")
     print(h)
 print("")
 #Future Hexagram
 print("Future Hexagram:")
 for x in seq:
     if x == 9:
-        h = ("_ _")
+        h = (" _______   _______\n/_______/ /_______/")
     elif x == 8:
-        h = ("_ _")
+        h = (" _______   _______\n/_______/ /_______/")
     elif x == 7:
-         h = ("___")
+         h = (" ________________\n/________________/")
     else:
-        h = ("___")
+        h = (" ________________\n/________________/")
     print(h)
